86db690912186d6b74b49b1e00a23a5880eebe40
nilldiggonto <nilldiggonto@gmail.com>
2020-11-15 02:49:44 +0600

    Details Page

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list,post_detail
 
 app_name = 'posts'
 urlpatterns = [
     path('post/',post_list,name='post_list'),
+    path('post/<slug:slug>/',post_detail,name='post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 # Create your views here.
 def post_list(request):
@@ -7,5 +7,14 @@ def post_list(request):
     context = {
         'object_list':queryset,
 
+    }
+    return render(request,template_name,context)
+
+
+def post_detail(request,slug):
+    instance = get_object_or_404(Post,slug=slug)
+    template_name = 'posts/detail.html'
+    context = {
+        'object':instance
     }
     return render(request,template_name,context)
