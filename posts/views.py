bb25e94a9547bb97b94ddfa4f20848b45350e46c
nilldiggonto <nilldiggonto@gmail.com>
2020-11-15 02:37:33 +0600

    Recipe List

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def post_list(request):
     template_name = 'posts/list.html'
-    return render(request,template_name)
+    queryset = Post.objects.all()
+    context = {
+        'object_list':queryset,
+
+    }
+    return render(request,template_name,context)
