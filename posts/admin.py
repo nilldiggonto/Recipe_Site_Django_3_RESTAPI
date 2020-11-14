51df11c71078549b13809ed777ecdb3f5442f40c
nilldiggonto <nilldiggonto@gmail.com>
2020-11-15 02:06:40 +0600

    Model Post

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','timestamp','updated']
+    prepopulated_fields = {'slug':('title',)}
+    list_display_links = ['title']
+    list_filter = ['updated','timestamp']
+    search_fields = ['title','content']
+    # list_editable = []
