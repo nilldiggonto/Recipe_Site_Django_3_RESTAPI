5b014be073dd5e319af04d6c67ae65686e6ce436
nilldiggonto <nilldiggonto@gmail.com>
2020-11-16 01:46:29 +0600

    Comments MVC

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from posts.models import Post
+from django.conf import settings
 # Create your models here.
+class Comment(models.Model):
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,default=1)
+    post        = models.ForeignKey(Post,on_delete= models.CASCADE,related_name='comment_post')
+    content     = models.TextField()
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
