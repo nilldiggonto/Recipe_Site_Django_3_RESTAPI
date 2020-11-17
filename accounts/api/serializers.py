3946142c961972d96f88004927d2a58732210d8f
nilldiggonto <nilldiggonto@gmail.com>
2020-11-18 00:03:50 +0600

    Accounts API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.serializers import HyperlinkedIdentityField,SerializerMethodField,ValidationError
 
 from django.contrib.auth import get_user_model
-
+from django.db.models import Q
 User = get_user_model()
 
 
@@ -63,8 +63,8 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
 ### LOGIN
 class UserLogiSerializer(serializers.ModelSerializer):
-    username = serializers.CharField()
-    email  = serializers.EmailField(label='Email')
+    username = serializers.CharField(allow_blank=True, required=False)
+    email  = serializers.EmailField(label='Email',allow_blank=True, required=False)
     token = serializers.CharField(allow_blank=True,read_only=True)
 
     class Meta:
@@ -77,3 +77,27 @@ class UserLogiSerializer(serializers.ModelSerializer):
 
         ]
         extra_kwargs = {'password': {'write_only':True}}
+
+    def validate(self,data):
+        user_obj = None
+        email = data.get('email',None)
+        username = data.get('username',None)
+        password = data['password']
+
+        if not email or not username:
+            raise ValidationError('Username or Email Required to login')
+
+        user = User.objects.filter(Q(email=email) | Q(username =username)).distinct()
+        user = user.exclude(email__isnull= True).exclude(email__iexact='')
+        if user.exists() and user.count() ==1:
+            user_obj = user.first()
+        else:
+            raise ValidationError('Username/Email not working')
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError('Incorrect information try again!')
+
+        data['token'] = 'RANDOM TOKEN'
+
+        return data
